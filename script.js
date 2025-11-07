242fbc715c433210083ac1b29b9e8c366d71e1bb
qahassan000 <qahassan000@gmail.com>
2025-11-07 15:17:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,51 +2,67 @@
 var CHART_WIDTH = 600;
 var CHART_HEIGHT = 600;
 
-
 d3.csv("vgsales.csv", function(error, data) {
     if (error) throw error;
 
+    // Convert strings to numbers
     data.forEach(function(d) {
-        d.Year = +d.Year;             // Convert to number
+        d.Year = +d.Year;
         d.Global_Sales = +d.Global_Sales;
-        d.NA_Sales = +d.NA_Sales;
-        d.EU_Sales = +d.EU_Sales;
-        d.JP_Sales = +d.JP_Sales;
     });
 
-    console.log(data); // Check that values are numbers now
+    // Group by Year and sum total sales per year
+    var salesByYear = d3.nest()
+        .key(function(d) { return d.Year; })
+        .rollup(function(v) {
+            return d3.sum(v, function(d) { return d.Global_Sales; });
+        })
+        .entries(data)
+        .map(function(d) {
+            return { Year: +d.key, TotalSales: d.values };
+        });
 
+    // Sort years ascending
+    salesByYear.sort(function(a, b) { return a.Year - b.Year; });
 
-    var xScale = d3.scale.linear(),
-        yScale = d3.scale.linear();
-    
-    xScale.domain([1980, 2020]).range([0, CHART_WIDTH]);
-    yScale.domain([0, d3.max(data, function(d){return d.Global_Sales;})]).range([CHART_HEIGHT, 0]);
-    
+    // Create scales
+    var xScale = d3.scale.linear()
+        .domain(d3.extent(salesByYear, function(d) { return d.Year; }))
+        .range([0, CHART_WIDTH]);
+
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(salesByYear, function(d) { return d.TotalSales; })])
+        .range([CHART_HEIGHT, 0]);
+
+    // Create line generator
     var lineGenerator = d3.svg.line()
-        .x(function(d){return xScale(d.Year); })
-        .y(function(d){return yScale(d.Global_Sales); });
-    
+        .x(function(d){ return xScale(d.Year); })
+        .y(function(d){ return yScale(d.TotalSales); });
+
     var g = d3.select("svg").select("g");
-    
-    var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(function(d){return "" + d;} ),
-        yAxis = d3.svg.axis().scale(yScale).orient("left");
-    
+
+    // Axes
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.format("d"));
+    var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
     g.append("g")
         .attr("class", "axis")
         .attr("transform", "translate(0," + CHART_HEIGHT + ")")
         .call(xAxis);
-    
+
     g.append("g")
         .attr("class", "axis")
         .call(yAxis);
-    
+
+    // Add line path
     g.append("path")
-        .style("fill", "none")
-        .style("stroke", "steelblue")
-        .style("stroke-width", "3px")
-        .attr("d", lineGenerator(data));
+        .datum(salesByYear)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 3)
+        .attr("d", lineGenerator);
 });
+
 
 
 
