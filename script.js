d2cf321a4fe3136f228f46d1e1c1a1771e0abe99
qahassan000 <qahassan000@gmail.com>
2025-11-07 10:53:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+var CHART_WIDTH = 500;
+var CHART_HEIGHT = 200;
+
+
 d3.csv("vgsales.csv", function(error, data) {
     if (error) throw error;
 
@@ -11,3 +16,41 @@ d3.csv("vgsales.csv", function(error, data) {
 
     console.log(data); // Check that values are numbers now
 });
+
+xScale = d3.scale.linear(),
+yScale = d3.scale.linear();
+
+xScale.domain(1980, 2020).range(0, CHART_WIDTH),
+xScale.domain(0, 700).range(CHART_HEIGHT, 0);
+
+var lineGenerator = d3.svg.line()
+    .x(function(d){return xScale(d.Year); })
+    .y(function(d){return yScale(d.Global_Sales); });
+
+g = d3.select("svg").select("g");
+
+xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(function(d) {return "" + d}; ),
+yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+g.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + CHART_HEIGHT + ")")
+    .call(xAxis);
+
+g.append("g")
+    .attr("class", "axis")
+    .call(yAxis);
+
+g.append("path")
+    .style("fill", "none")
+    .style("stroke", "steelblue")
+    .style("stroke-width", "3px")
+    .attr("d", lineGenerator(data));
+
+
+
+
+
+
+
+
