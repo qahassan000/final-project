98ae0fb45af43ac8546bba4139af2707d33e2c0b
qahassan000 <qahassan000@gmail.com>
2025-11-15 09:26:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ d3.csv("vgsales.csv", function(error, data) {
 
     // Create line generator
     var lineGenerator = d3.svg.line()
-        .x(function(d){ return xScale(d.fiveYear) + xScale.rangeBand(); })
+        .x(function(d){ return xScale(d.fiveYear) + xScale.rangeBand() / 2; })
         .y(function(d){ return yScale(d.TotalSales); });
     
 
